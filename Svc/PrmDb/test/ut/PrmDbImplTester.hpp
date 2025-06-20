9ddecc9e34a68e84b7a579f491d348b3c8a20ee5
Mishaal Aleem <mishaalaleem@gmail.com>
2025-06-20 14:41:19 -0700

    Updates for Svc/PrmDb corresponding to UT cmake udpate for protected/private

--- Svc/PrmDb/test/ut/PrmDbImplTester.hpp
+++ Svc/PrmDb/test/ut/PrmDbImplTester.hpp
@@ -1,78 +1,55 @@
 /*
  * PrmDbImplTester.hpp
  *
- *  Created on: Mar 18, 2015
- *      Author: tcanham
+ *  Created on: Jun 20, 2025
+ *      Author: aleem
  */
 
-#ifndef PRMDB_TEST_UT_PRMDBIMPLTESTER_HPP_
-#define PRMDB_TEST_UT_PRMDBIMPLTESTER_HPP_
+#ifndef PRMDB_TEST_UT_PRMDBTESTER_IMPL_HPP_
+#define PRMDB_TEST_UT_PRMDBTESTER_IMPL_HPP_
 
-#include <PrmDbGTestBase.hpp>
-#include <config/PrmDbImplTesterCfg.hpp>
 #include <Svc/PrmDb/PrmDbImpl.hpp>
-#include <Os/Stub/test/File.hpp>
 
 namespace Svc {
 
-    class PrmDbImplTester : public PrmDbGTestBase {
+    /**
+     * @brief Wrapper class for PrmDbImpl to expose private methods for testing
+     *
+     * This class exists to provide access to private methods of PrmDbImpl without
+     * requiring additional friendship declarations. It leverages the existing
+     * friendship between PrmDbImpl and PrmDbImplTester.
+     */
+    class PrmDbImplTester {
         public:
-            PrmDbImplTester(Svc::PrmDbImpl& inst);
+            /**
+             * Constructor
+             */
+            PrmDbImplTester(PrmDbImpl& impl);
+
+            /**
+             * Destructor
+             */
             virtual ~PrmDbImplTester();
 
-            void runNominalPopulate();
-            void runNominalSaveFile();
-            void runNominalLoadFile();
-            void runMissingExtraParams();
-            void runFileReadError();
-            void runFileWriteError();
+            /**
+             * Clear the parameter database
+             */
+            void clearDb();
 
-            void runRefPrmFile();
+            // /**
+            //  * Access to doDispatch method
+            //  */
+            // Fw::QueuedComponentBase::MsgDispatchStatus doDispatch();
+
+            /**
+             * Access to readParamFile method
+             */
+            void readParamFile();
+
         private:
-            //! Handler for from_pingOut
-            //!
-            void from_pingOut_handler(
-                const FwIndexType portNum, /*!< The port number*/
-                U32 key /*!< Value to return to pinger*/
-            );
-            Svc::PrmDbImpl& m_impl;
-            void resetEvents();
-
-
-
-            // enumeration to tell what kind of error to inject
-            enum ErrorType {
-                FILE_STATUS_ERROR, // return a bad read status
-                FILE_SIZE_ERROR, // return a bad size
-                FILE_DATA_ERROR,  // return unexpected data
-                FILE_READ_NO_ERROR,    // No error
-            };
-            Os::File::Status m_status;
-            FwSizeType m_waits = 0;
-            ErrorType m_errorType = FILE_READ_NO_ERROR;
-
-            BYTE m_io_data[PRMDB_IMPL_TESTER_MAX_READ_BUFFER];
-
-            // write call modifiers
-
-            Os::File::Status WriteInterceptor();
-            Os::File::Status m_testWriteStatus;
-
-      public:
-        class PrmDbTestFile : public Os::Stub::File::Test::TestFile {
-          public:
-            Status read(U8 *buffer, FwSizeType &size, WaitType wait) override;
-
-            Status write(const U8 *buffer, FwSizeType &size, WaitType wait) override;
-
-            // Tracks the current tester
-            static void setTester(PrmDbImplTester* tester);
-            static PrmDbImplTester* s_tester;
-
-        };
-
+            PrmDbImpl& m_impl; //!< Reference to implementation being tested
     };
 
-} /* namespace SvcTest */
+} /* namespace Svc */
 
-#endif /* PRMDB_TEST_UT_PRMDBIMPLTESTER_HPP_ */
+#endif /* PRMDB_TEST_UT_PRMDBTESTER_IMPL_HPP_ */
